15b12c95c3ba1eda427aa499a5d5f63923fef596
Ndacyayisenga-droid <ndacyayinoah@gmail.com>
2024-07-28 23:31:11 +0300

    Configure ports

--- start-all-servers.sh
+++ start-all-servers.sh
@@ -2,58 +2,52 @@
 
 # Define protocols and ports for the servers
 PROTOCOL=https
-GAME1_SERVER_PORT=8443
-GAME1_FRONTEND_PORT=3001
-GAME2_SERVER_PORT=8444
-GAME2_FRONTEND_PORT=3002
-GENERAL_SERVER_PORT=8445
-GENERAL_FRONTEND_PORT=3003
 
 # Start game1-server
-echo "Starting game1-server on port $GAME1_SERVER_PORT"
+echo "Starting game1-server"
 cd needfortoken
-mvn clean install
-mvn spring-boot:run -Dspring-boot.run.arguments=--server.port=$GAME1_SERVER_PORT &
+./mvnw clean verify
+./mvnw spring-boot:run &
 GAME1_SERVER_PID=$!
 cd ..
 
 # Start game1-frontend
-echo "Starting game1-frontend on port $GAME1_FRONTEND_PORT"
+echo "Starting game1-frontend"
 cd needfortoken-frontend
 npm install
-npm run dev -- --port $GAME1_FRONTEND_PORT &
+npm run dev &
 GAME1_FRONTEND_PID=$!
 cd ..
 
 # Start game2-server
-echo "Starting game2-server on port $GAME2_SERVER_PORT"
+echo "Starting game2-server"
 cd tokemon
-mvn clean install
-mvn spring-boot:run -Dspring-boot.run.arguments=--server.port=$GAME2_SERVER_PORT &
+./mvnw clean verify
+./mvnw spring-boot:run &
 GAME2_SERVER_PID=$!
 cd ..
 
 # Start game2-frontend
-echo "Starting game2-frontend on port $GAME2_FRONTEND_PORT"
+echo "Starting game2-frontend"
 cd tokemon-frontend
 npm install
-npm run dev -- --port $GAME2_FRONTEND_PORT &
+npm run dev &
 GAME2_FRONTEND_PID=$!
 cd ..
 
 # Start general-server
-echo "Starting general-server on port $GENERAL_SERVER_PORT"
+echo "Starting general-server"
 cd Noobisoft-server
-mvn clean install
-mvn spring-boot:run -Dspring-boot.run.arguments=--server.port=$GENERAL_SERVER_PORT &
+./mvnw clean verify
+./mvnw spring-boot:run &
 GENERAL_SERVER_PID=$!
 cd ..
 
 # Start general-frontend
-echo "Starting general-frontend on port $GENERAL_FRONTEND_PORT"
+echo "Starting general-frontend"
 cd noobisoft-frontend
 npm install
-npm run dev -- --port $GENERAL_FRONTEND_PORT &
+npm run dev &
 GENERAL_FRONTEND_PID=$!
 cd ..
 
