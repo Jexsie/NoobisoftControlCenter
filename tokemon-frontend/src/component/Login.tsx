ec1ebdbe56c6f44e449647d146e6770b09fa4fa5
jexsie <jessiessebuliba@gmail.com>
2024-08-02 13:51:20 +0300

    add frontend uis

--- tokemon-frontend/src/component/Login.tsx
+++ tokemon-frontend/src/component/Login.tsx
@@ -1,6 +1,9 @@
 export default function Login() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
+    <div
+      className="w-full h-screen flex justify-center items-center"
+      style={{ backdropFilter: "blur(25px)" }}
+    >
       <div
         style={{ height: "60vh" }}
         className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700"
@@ -40,31 +43,6 @@ export default function Login() {
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
               required
             />
-          </div>
-          <div className="flex items-start">
-            <div className="flex items-start">
-              <div className="flex items-center h-5">
-                <input
-                  id="remember"
-                  type="checkbox"
-                  value=""
-                  className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                  required
-                />
-              </div>
-              <label
-                id="remember"
-                className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
-                Remember me
-              </label>
-            </div>
-            <a
-              href="#"
-              className="ms-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
-            >
-              Lost Password?
-            </a>
           </div>
           <button
             type="submit"
