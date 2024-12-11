cfb9352efd15bda77ed1cf0f76180d08a358798a
Jessy Ssebuliba <jessiessebuliba@gmail.com>
2024-12-11 11:56:20 +0300

    remove associate button

--- tokemon-frontend/src/component/Login.tsx
+++ tokemon-frontend/src/component/Login.tsx
@@ -114,12 +114,6 @@ export default function Login({ isOpen, setIsOpen }) {
           >
             Connect wallet
           </button>
-          <button
-            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            onClick={associateTx}
-          >
-            Associate
-          </button>
         </div>
       </div>
     </div>
